5f9f54c96a6d75214c2ee575c67ca7e2030c5bbc
Michal Sak <michusakowicz@gmail.com>
2023-08-10 12:41:50 +0200

    Add first tests

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,23 @@
+export const convertPLNToUSD = (PLN) => {
+
+  const PLNtoUSD = PLN / 3.5;
+  
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN;
+  }
+
+  if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  }
+
+  if (PLN < 0) {
+    return '$0.00';
+  }
+  
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
